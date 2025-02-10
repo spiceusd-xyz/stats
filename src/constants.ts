b824df112b3f3678ac7ce7b5156ae003b8a3210d
Aaron Jen <aaron821124@gmail.com>
2025-02-10 17:02:02 +0800

    update blast dune url

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const OUTPUT_DIR_V2 = path.join(OUTPUT_DIR, "v2");
 
 export const DUNE_SPV2_AVERAGE_APY_URL_MAINNET = "https://api.dune.com/api/v1/query/4412077/results";
 export const DUNE_SPV2_AVERAGE_APY_URL_SEPOLIA = null;
-export const DUNE_SPV2_AVERAGE_APY_URL_BLAST = null;
+export const DUNE_SPV2_AVERAGE_APY_URL_BLAST = "https://api.dune.com/api/v1/query/4680052/results";
 export const DUNE_URLS = {
   mainnet: DUNE_SPV2_AVERAGE_APY_URL_MAINNET,
   sepolia: DUNE_SPV2_AVERAGE_APY_URL_SEPOLIA,
